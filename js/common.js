14179e9e0bfac33f220104bebed84472a78f1a77
EJP <obliviscor29@naver.com>
2023-07-04 17:31:06 +0900

    모바일 메뉴 했는데 등장이벤트 안돼서 나중에 이부분 다시

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 // common.js
 
-// 헤더영역 색상변경
+// 헤더 색상변경
 $('#header').mouseover(function(){
     $(this).css({
         backgroundColor : '#fff'
@@ -11,19 +11,32 @@ $('#header').mouseover(function(){
     });
 });
 
+// 모바일 메뉴 열기
+$('.menuBtn').click(function(){
+    $('.menu_open').show();
+    return false;
+});
+
+
+// 모바일 메뉴 닫기
+$('.menu_close').click(function(){
+    $('.menu_open').hide();
+    return false;
+});
+
 // 모바일 아코디언 메뉴
 $('.gnb .d1').click(function(){
     $(this).find('.sub').stop().slideToggle();
     // "가상태그로 만든 화살표.. 부드럽게 회전하고 싶은데 안됩니다..ㅜ"
     $(this).find('.m1').toggleClass('on');
+    $('#header').css({
+        backgroundColor : '#191C20'
+    });
     return false;
 });
 
 // PC gnb 서브메뉴
 $('.pc_gnb .m1').mouseover(function(){
-    $(this).animate({
-        textDecorate : 'underline'
-    });
     $('.pc_gnb_bg').stop().slideDown();
     $(this).next().stop().slideDown();
 
