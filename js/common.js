6942a9229fe517439e1a862d1dc3a1d931b736ed
EJP <obliviscor29@naver.com>
2023-07-04 16:24:57 +0900

    아코디언 js ok

--- js/common.js
+++ js/common.js
@@ -11,6 +11,14 @@ $('#header').mouseover(function(){
     });
 });
 
+// 모바일 아코디언 메뉴
+$('.gnb .d1').click(function(){
+    $(this).find('.sub').stop().slideToggle();
+    // "가상태그로 만든 화살표.. 부드럽게 회전하고 싶은데 안됩니다..ㅜ"
+    $(this).find('.m1').toggleClass('on');
+    return false;
+});
+
 // PC gnb 서브메뉴
 $('.pc_gnb .m1').mouseover(function(){
     $(this).animate({
