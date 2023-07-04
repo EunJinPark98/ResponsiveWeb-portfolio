9ee1a1a724104d43aa7ff92fa6d10208a63129c1
EJP <obliviscor29@naver.com>
2023-07-04 15:21:18 +0900

    pc_gnb 수정햇음

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 // common.js
 
 // PC gnb 서브메뉴
+$('.pc_gnb .m1').mouseover(function(){
+    $(this).animate({
+        textDecorate : 'underline'
+    });
+    $('.pc_gnb_bg').stop().slideDown();
+    $(this).next().stop().slideDown();
+
+}).mouseout(function(){
+    $('.pc_gnb_bg').stop().slideUp();
+    $(this).next().stop().slideUp();
+});
+
