d8b5190c43088ef63845cc25fde89642bd47373f
EJP <obliviscor29@naver.com>
2023-07-04 15:31:55 +0900

    PC 헤더 ok

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 // common.js
 
+// 헤더영역 색상변경
+$('#header').mouseover(function(){
+    $(this).css({
+        backgroundColor : '#fff'
+    });
+}).mouseout(function(){
+    $(this).css({
+        backgroundColor : 'transparent'
+    });
+});
+
 // PC gnb 서브메뉴
 $('.pc_gnb .m1').mouseover(function(){
     $(this).animate({
