b3dd5f9337f955e0319ad8e2e87220777a6b6159
EJP <obliviscor29@naver.com>
2023-07-06 15:17:16 +0900

    슬라이드  컨텐츠 넘어가게

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 // main.js
 
-// 본문 1 슬라이드
+// main 1 슬라이드
 $('.m1_slide').slick({
-    dots : true,
-    appendDots : '.ms_pager',
-    customPaging : function(slider, i){
-      return i+1 + ' · ' + slider.slideCount;
-    },
-    prevArrow : '.m1_prev',
-    nextArrow : '.m1_next',
-    autoplay : true,
-    atoplaySpeed : 500,
-    speed : 1000,
+  dots : true,
+  appendDots : '.ms_pager',
+  customPaging : function(slider, i){
+    return i+1 + ' · ' + slider.slideCount;
+  },
+  prevArrow : '.m1_prev',
+  nextArrow : '.m1_next',
+  autoplay : true,
+  atoplaySpeed : 500,
+  speed : 1000,
+});
+// main 1 슬라이드 텍스트
+$('.m1_text .on').fadeIn(1000);
+
+$('.m1_slide').on('afterChange', function(event, slick, currentSlide) {
+  console.log(currentSlide);
+  $('.m1_txt_box').removeClass('on').hide();
+  $('.m1_txt_box').eq(currentSlide).addClass('on').fadeIn(1000);
 });
 
-$('.m1_pause').click(function(){
-  $(this).toggle();
-  $(this).siblings('.m1_play').show();
-});
+// main 1 슬라이드 재생버튼
 $('.m1_play').click(function(){
-  $(this).toggle();
-  $(this).siblings('.m1_pause').show();
+  $('.m1_slide').slick('slickPlay');
+  $(this).hide();
+  $('.m1_pause').show();
+});
+
+// main 1 슬라이드 멈춤버튼
+$('.m1_pause').click(function(){
+  $('.m1_slide').slick('slickPause');
+  $(this).hide();
+  $('.m1_play').show();
 });
