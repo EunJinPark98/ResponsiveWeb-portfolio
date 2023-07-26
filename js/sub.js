0a2e9862de7b001c3c8c08abbf07fa69cc84a9fa
EJP <obliviscor29@naver.com>
2023-07-26 14:24:58 +0900

    개발자 스토리 탭

--- js/sub.js
+++ js/sub.js
@@ -1,1 +1,11 @@
 // sub.js
+
+// developer.html
+// 개발자 스토리 탭
+$('.storyTab li').click(function(){
+    $('.storyTab li').removeClass('on');
+    $(this).addClass('on');
+    let i = $(this).index();
+    $('.storyBoard > div').removeClass('active');
+    $('.storyBoard > div').eq(i).addClass('active');
+});
