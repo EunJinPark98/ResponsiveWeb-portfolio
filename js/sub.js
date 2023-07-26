448411486c8f6e6846efb3024f6b2279334e9242
EJP <obliviscor29@naver.com>
2023-07-26 17:48:01 +0900

    개발블로그 영역 등장하는 js 하다말았음 sub.js

--- js/sub.js
+++ js/sub.js
@@ -1,4 +1,22 @@
 // sub.js
+$(window).scroll(function(){
+    let st = $(window).scrollTop();
+    let blogStart = $('.developer_techBlog').offset().top;
+    let blogEnd = blogStart + $('.developer_techBlog').height() * 0.4;
+    if(st>=blogStart && st<=blogEnd){
+        $('.blogBox2').css({
+            transform : 'translateY(0)',
+            opacity : 1,
+            transition : '1.5s'
+        })
+    }else{
+        $('.blogBox2').css({
+            transform : 'translateY(200px)',
+            opacity : 0,
+            transition : '1.5s'
+        })
+    }
+});
 
 // 개발자 - 스토리 탭
 $('.storyTab li').click(function(){
@@ -8,3 +26,4 @@ $('.storyTab li').click(function(){
     $('.storyBoard > div').removeClass('active');
     $('.storyBoard > div').eq(i).addClass('active');
 });
+
